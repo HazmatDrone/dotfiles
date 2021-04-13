2cfe896ad26e5f1be0302ecb482f9225b3247887
HazmatDrone <drone@hazmatdr.one>
2021-04-14 02:35:47 +1000

    Get correct $ZSH var

--- .zsh_install_plugins.sh
+++ .zsh_install_plugins.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-if [[ ! -d "$HOME/.tmux/plugins/tpm" ]]; then
-    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-fi
-
-if [[ ! (-d "$HOME/.oh-my-zsh" || -d "/usr/share/oh-my-zsh") ]]; then
-	git clone https://github.com/robbyrussell/oh-my-zsh $HOME/.oh-my-zsh
+if [ -d "/usr/share/oh-my-zsh" ]; then
+	export ZSH="/usr/share/oh-my-zsh"
+else
+	if [[ ! (-d "$HOME/.oh-my-zsh" || -d "/usr/share/oh-my-zsh") ]]; then
+		git clone https://github.com/robbyrussell/oh-my-zsh $HOME/.oh-my-zsh
+	fi
+	export ZSH="$HOME/.oh-my-zsh"
 fi
 
 if [ ! -d "${ZSH_CUSTOM}/plugins/zsh-syntax-highlighting" ]; then
 	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+fi
+
+if [[ ! -d "$HOME/.tmux/plugins/tpm" ]]; then
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 fi
